fae5bdc3d4a998f085dee3963cc1fb5a09a03e2b
rushikeshbhandare <86002633+rushikeshbhandare97@users.noreply.github.com>
2021-08-30 10:52:21 +0530

    Create DiceNumber.js

---
+++ DiceNumber.js
@@ -0,0 +1,4 @@
+//Question-2 Random to get dice number between 1 to 6.
+
+let diceNumber = Math.floor(Math.random() * (6 - 1)) + 1;
+console.log("Generating dice numbers between 1 to 6 : " + diceNumber);
